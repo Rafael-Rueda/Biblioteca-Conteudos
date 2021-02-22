ddb49937befd8c70a01314cec1f4642aab848e82
Rafael-Rueda <62863872+Rafael-Rueda@users.noreply.github.com>
2021-02-22 04:29:49 -0300

    Add files via upload

---
+++ LI Margin.js
@@ -0,0 +1,30 @@
+// CASO QUEIRA FAZER A MARGEM ENTRE OS ITENS POR JAVASCRIPT:
+
+/*
+(function() {
+    var navitem = document.getElementsByClassName('navlista').item(0).firstElementChild;
+    for (c = 0; c <= 4 ; c++) {
+        var valortop = c * 10;
+        switch (valortop) {
+            case 0:
+                navitem.style = "top: 0px;";
+                break ;
+            case 10:
+                navitem.style = "top: 10px;";
+                break;
+            case 20:
+                navitem.style = "top: 20px;";
+                break;
+            case 30:
+                navitem.style = "top: 30px;";
+                break;
+            case 40:
+                navitem.style = "top: 40px;";
+                break;
+            default:
+                break;
+        }
+        var navitem = navitem.nextElementSibling
+    }
+} () )
+*/
