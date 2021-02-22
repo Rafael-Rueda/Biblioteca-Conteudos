16e31b87a457a56f31dabf7aa86b707e8a723e02
Rafael-Rueda <62863872+Rafael-Rueda@users.noreply.github.com>
2021-02-22 04:28:27 -0300

    Add files via upload

---
+++ reiniciar_calculadora.js
@@ -0,0 +1,10 @@
+linhas_calculadora.item(0).firstElementChild.nextElementSibling.addEventListener('click', function () {
+    conta = 0;
+    conta2 = 0;
+    it = 0;
+    cancelar = false;
+    igual = false;
+    codigo_calculadora = ''
+    document.getElementById('resultado_calculadora').firstChild.nodeValue = '0'
+    document.getElementById('userview_calculadora').firstChild.nodeValue = ' '
+})
