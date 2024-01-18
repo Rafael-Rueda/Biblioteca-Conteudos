a07b503a7b9818095829d913207522984530fcda
Rafael <rafael.rued4@gmail.com>
2024-01-18 01:12:31 -0300

    clouds upd

--- Assets/React-Components/Outros/clouds/index.jsx
+++ Assets/React-Components/Outros/clouds/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { ReactComponent as CloudsSVG } from './images/clouds.svg';
+import { ReactComponent as CloudsSVG } from './images/clouds2.svg';
 import styles from './styles/styles.module.css';
 
 export function Clouds() {
@@ -7,8 +7,8 @@ export function Clouds() {
     useEffect(() => {
         const $cloud1 = document.getElementById('cloud__01__svg');
         const $cloud2 = document.getElementById('cloud__02__svg');
-        const $cloud3 = document.getElementById('cloud__03__svg');
-        const $cloud4 = document.getElementById('cloud__04__svg');
+        // const $cloud3 = document.getElementById('cloud__03__svg');
+        // const $cloud4 = document.getElementById('cloud__04__svg');
 
         function getCurrentTranslate(el) {
             const computedStyle = window.getComputedStyle(el);
@@ -29,10 +29,11 @@ export function Clouds() {
 
             if (position < 408 && position > -380) {
                 el.style.transition = 'transform 0.7s ease';
-                el.style.transform = `translate(${window.innerWidth > 1300 ? (lr === 'left' ? -120 : 150) : (lr === 'left' ? -250 : 250)}px, ${currentY}px)`;
+                el.style.transform = `translate(${window.innerWidth > 1300 ? (lr === 'left' ? -150 : 150) : (lr === 'left' ? -250 : 250)}px, ${currentY}px)`;
             } else {
                 el.style.transition = 'transform 0.7s ease';
-                el.style.transform = `translate(24.2px, ${currentY}px)`;
+                el.style.transform = `translate(0px, ${currentY}px)`;
+                // el.style.transform = `translate(24.2px, ${currentY}px)`;
             };
         };
 
@@ -42,7 +43,7 @@ export function Clouds() {
             // Use these lines instead of "moveElement()" if the moveElement function turns complex and slow.
 
             // let [currentX, currentY] = getCurrentTranslate(el);
-            // if ((position < 408 && position > -380) || (el.style.transform !== `translate(24.2px, ${currentY}px)`)) { moveElement(el, position, lr);}
+            // if ((position < 408 && position > -380) || (el.style.transform !== `translate(0px, ${currentY}px)`)) { moveElement(el, position, lr);}
             moveElement(el, position, lr);
         };
 
@@ -59,8 +60,8 @@ export function Clouds() {
 
             elementCalc($cloud1, 'left');
             elementCalc($cloud2, 'right');
-            elementCalc($cloud3, 'left');
-            elementCalc($cloud4, 'right');
+            // elementCalc($cloud3, 'left');
+            // elementCalc($cloud4, 'right');
         };
 
         window.addEventListener('scroll', handleScroll);
