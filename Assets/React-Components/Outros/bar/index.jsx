9e77852f39cd2f13273c5cc6ce062c8bb2c29e10
Rafael <rafael.rued4@gmail.com>
2024-01-22 16:13:15 -0300

    bar

--- Assets/React-Components/Outros/bar/index.jsx
+++ Assets/React-Components/Outros/bar/index.jsx
@@ -3,43 +3,39 @@ import styles from './styles/styles.module.css';
 
 export function Bar() {
     const el = useRef(null);
+    const skills = useRef(null);
 
     useEffect(() => {
         function animation() {
             const balls = document.getElementsByClassName(styles.ball);
 
-            const window900 = window.matchMedia('(max-width: 900px)').matches;
+            console.log(el.current.offsetHeight);
+            el.current.style.height = `${window.scrollY - 600 < 0 ? 0 : window.scrollY - 600 > skills.current.offsetHeight ? skills.current.offsetHeight : window.scrollY - 600}px`
+            // If I want the ball movement effect:
+            // el.current.style.transform = `translate(-50%, ${window.scrollY - 360 < 0 ? 0 : window.scrollY - 360 > 780 ? 780 : window.scrollY - 360}px)`
 
-            if (!window900) {
-                el.current.style.height = `${window.scrollY - 600 < 0 ? 0 : window.scrollY - 600 > 800 ? 800 : window.scrollY - 600}px`
-                // If I want the ball movement effect:
-                // el.current.style.transform = `translate(-50%, ${window.scrollY - 360 < 0 ? 0 : window.scrollY - 360 > 780 ? 780 : window.scrollY - 360}px)`
-
-                if ((window.scrollY - 700) >= -60) {
-                    balls[3].style.backgroundColor = 'var(--skill-bar)';
-                } else {
-                    balls[3].style.backgroundColor = 'var(--text-color)';
-                }
-
-                if ((window.scrollY - 700) >= 120) {
-                    balls[0].style.backgroundColor = 'var(--skill-bar)';
-                } else {
-                    balls[0].style.backgroundColor = 'var(--text-color)';
-                }
-
-                if ((window.scrollY - 700) >= 270) {
-                    balls[1].style.backgroundColor = 'var(--skill-bar)';
-                } else {
-                    balls[1].style.backgroundColor = 'var(--text-color)';
-                }
-
-                if ((window.scrollY - 700) >= 500) {
-                    balls[2].style.backgroundColor = 'var(--skill-bar)';
-                } else {
-                    balls[2].style.backgroundColor = 'var(--text-color)';
-                }
+            if ((window.scrollY - 700) >= -60) {
+                balls[3].style.backgroundColor = 'var(--skill-bar)';
             } else {
-                el.current.style.height = `${window.scrollY - 700 < 0 ? 0 : window.scrollY - 700 > 780 ? 780 : window.scrollY - 700}px`
+                balls[3].style.backgroundColor = 'var(--text-color)';
+            }
+
+            if ((window.scrollY - 700) >= 120) {
+                balls[0].style.backgroundColor = 'var(--skill-bar)';
+            } else {
+                balls[0].style.backgroundColor = 'var(--text-color)';
+            }
+
+            if ((window.scrollY - 700) >= 270) {
+                balls[1].style.backgroundColor = 'var(--skill-bar)';
+            } else {
+                balls[1].style.backgroundColor = 'var(--text-color)';
+            }
+
+            if ((window.scrollY - 700) >= 500) {
+                balls[2].style.backgroundColor = 'var(--skill-bar)';
+            } else {
+                balls[2].style.backgroundColor = 'var(--text-color)';
             }
         }
 
@@ -55,7 +51,7 @@ export function Bar() {
     }, []);
 
     return (
-        <div className={styles.skillsbar}>
+        <div ref={skills} className={styles.skillsbar}>
             <div ref={el} className={styles.circle}></div>
 
             <div className={styles.ball}></div>
@@ -64,8 +60,6 @@ export function Bar() {
             <div className={styles.ball}></div>
 
             <div className={styles.bar}></div>
-
-            
         </div>
     )
 }
