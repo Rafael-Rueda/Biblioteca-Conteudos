acfef712ffa6330adbeafe4bfc70b3d265ed5bcc
Rafael <rafael.rued4@gmail.com>
2024-01-26 01:37:00 -0300

    scrollbar

--- Assets/React-Components/Outros/scrollBar01/index.jsx
+++ Assets/React-Components/Outros/scrollBar01/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 import styles from './styles/styles.module.css';
 
-export function Bar() {
+export function ScrollBar01() {
     const el = useRef(null);
     const skills = useRef(null);
 
@@ -9,30 +9,37 @@ export function Bar() {
         function animation() {
             const balls = document.getElementsByClassName(styles.ball);
 
-            console.log(el.current.offsetHeight);
-            el.current.style.height = `${window.scrollY - 600 < 0 ? 0 : window.scrollY - 600 > skills.current.offsetHeight ? skills.current.offsetHeight : window.scrollY - 600}px`
+            const $elHeight = ((window.scrollY + 372) - (skills.current.getBoundingClientRect().top + window.scrollY));
+            el.current.style.height = `${$elHeight < 0 ? 0 : $elHeight > skills.current.offsetHeight ? skills.current.offsetHeight : $elHeight}px`;
+
+            // If i want to manually (not dinamically) set the height of the scrollbar in the page
+            // el.current.style.height = `${window.scrollY - 600 < 0 ? 0 : window.scrollY - 600 > skills.current.offsetHeight ? skills.current.offsetHeight : window.scrollY - 600}px`
+
             // If I want the ball movement effect:
             // el.current.style.transform = `translate(-50%, ${window.scrollY - 360 < 0 ? 0 : window.scrollY - 360 > 780 ? 780 : window.scrollY - 360}px)`
 
-            if ((window.scrollY - 700) >= -60) {
+            // el.current.style.height = `${window.scrollY}`
+
+
+            if (($elHeight - 100) >= -60) {
                 balls[3].style.backgroundColor = 'var(--skill-bar)';
             } else {
                 balls[3].style.backgroundColor = 'var(--text-color)';
             }
 
-            if ((window.scrollY - 700) >= 120) {
+            if (($elHeight - 100) >= 145) {
                 balls[0].style.backgroundColor = 'var(--skill-bar)';
             } else {
                 balls[0].style.backgroundColor = 'var(--text-color)';
             }
 
-            if ((window.scrollY - 700) >= 270) {
+            if (($elHeight - 100) >= 320) {
                 balls[1].style.backgroundColor = 'var(--skill-bar)';
             } else {
                 balls[1].style.backgroundColor = 'var(--text-color)';
             }
 
-            if ((window.scrollY - 700) >= 500) {
+            if (($elHeight - 100) >= 585) {
                 balls[2].style.backgroundColor = 'var(--skill-bar)';
             } else {
                 balls[2].style.backgroundColor = 'var(--text-color)';
